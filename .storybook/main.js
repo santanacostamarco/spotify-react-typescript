788ca2d6b88ff501f40e36074b08f9a114b56e26
Marco Santana <santannacostamarco@gmail.com>
2021-07-04 20:08:47 -0300

    config storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.story.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
---
+++ src/App.story.tsx
@@ -0,0 +1,9 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import App from './App'
+
+export default {
+  title: 'App',
+  component: App
+} as Meta
+
+export const Default: Story = () => <App />
